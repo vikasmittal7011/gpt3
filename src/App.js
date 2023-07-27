b9868d7d6c0d74b9a4f08a8a22e77571dc6fb10d
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-27 21:10:55 +0530

    Add basic css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Suspense } from "react";
+
+import "./app.css";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGTP3,
+} from "./containers/index";
+import { NavBar, CTA, Brand } from "./components/index";
 
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="App">
+        <div className="gradient__bg">
+          <NavBar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGTP3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
+    </Suspense>
+  );
 };
 
 export default App;
