814d60ff8f2ba8c20474471679de24897fe02489
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-28 18:10:00 +0530

    Complete WhatGPT3

--- src/containers/whatGTP3/WhatGTP3.js
+++ src/containers/whatGTP3/WhatGTP3.js
@@ -1,9 +1,39 @@
 import React from "react";
 
+import Feature from "../../components/feature/Feature";
 import "./whatGTP3.css";
 
 const WhatGTP3 = () => {
-  return <div>WhatGTP3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgtp">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="gtp3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The Possibility are beyound your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGTP3;
