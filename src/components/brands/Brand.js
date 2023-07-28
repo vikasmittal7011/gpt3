be9f3b266534f2bd70f732e527d6061b7dd333c4
vikas gupta <vikasaggrawal700@gmail.com>
2023-07-28 16:40:22 +0530

    Complete brand

--- src/components/brands/Brand.js
+++ src/components/brands/Brand.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import "./brand.css"
+import "./brand.css";
+import { google, slack, atlassian, shopify, dropbox } from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gtp3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
